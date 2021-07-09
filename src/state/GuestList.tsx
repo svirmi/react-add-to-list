ce3146ba65521e6e5f396f049153195005be2ed1
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-07-09 09:06:24 +0300

    set variable type , render list

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 const GuestList: React.FC = () => {
     
     const [name, setName] = useState('');
-    const [guests, setGuests] = useState([]);
+    const [guests, setGuests] = useState<string[]>([]);
 
     const onClick = () => {
         setName('');
@@ -12,6 +12,12 @@ const GuestList: React.FC = () => {
     
     return <div>
         <h3>Guest List</h3>
+
+        <ul>
+            {guests.map(
+                guest => <li key={guest}>{guest}</li>
+            )}
+        </ul>
 
         <input value={name} onChange={(e) => setName(e.target.value)} />
         <button onClick={onClick} >Add Guest</button>
