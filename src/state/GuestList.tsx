063cf3fc3983b81a63d1ba40c7f71ae6efca3eb5
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-07-09 08:45:16 +0300

    initial setup

---
+++ src/state/GuestList.tsx
@@ -0,0 +1,10 @@
+const GuestList: React.FC = () => {
+    return <div>
+        <h3>Guest List</h3>
+
+        <input />
+        <button>Add Guest</button>
+    </div>
+}
+
+export default GuestList;
