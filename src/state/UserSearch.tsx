1595b676a95975c5576532227bbeaad9e068e15a
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-07-11 19:49:42 +0300

    search and show user's data

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -10,13 +10,24 @@ const users = [
 const UserSearch: React.FC = () => {
 
     const [name, setName] = useState('');
+    const [user,setUser] = useState<{name: string, age: number} | undefined>();
     
-    const onClick = () => {}
+    const onClick = () => {
+        const foundUser = users.find((user) => {
+            return user.name === name;
+        });
+
+        setUser(foundUser);
+    }
 
     return <div>
         <h3>User Search</h3>
         <input value={name} onChange={e => setName(e.target.value)} />
-        <button>Find User</button>
+        <button onClick={onClick}>Find User</button>
+        <div>
+            {user && user.name}
+            {user && user.age}
+        </div>
     </div>
 }
 
