9d56de7305b073b64cd79456a055da796871b3d0
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-07-14 09:57:22 +0300

    UserSearch as class-based component

--- src/classes/UserSearch.tsx
+++ src/classes/UserSearch.tsx
@@ -1,7 +1,57 @@
 import { Component } from "react";
 
-class UserSearch extends Component {
+interface User {
+    name: string;
+    age: number;
+}
 
+interface UserSearchProps {
+    users: User[] 
+}
+
+interface UserSearchState {
+    name: string;
+    user: User | undefined
+}
+
+class UserSearch extends Component<UserSearchProps> {
+
+        // this.props.users
+        // this.state.name
+        // this.state.user
+
+    state: UserSearchState = {
+        name: '',
+        user: undefined
+    }
+
+    onClick = () => {
+        const foundUser = this.props.users.find((user) => {
+            return user.name === this.state.name;
+        });
+
+        this.setState({user: foundUser})
+    }
+
+    render() {
+
+        const { name, user } = this.state;
+
+        return (
+            <div>
+                <h3>User Search</h3>
+                <input 
+                    value={name} 
+                    onChange={(e) => this.setState({name: e.target.value})} 
+                />
+                <button onClick={this.onClick}>Find User</button>
+                <div>
+                    {user && user.name}
+                    {user && user.age}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default UserSearch
