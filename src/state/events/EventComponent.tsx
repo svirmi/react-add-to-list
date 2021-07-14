b418c4ed1af25db21ec082a286beffd84d94166d
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2021-07-14 09:12:46 +0300

    standalone event handler

--- src/state/events/EventComponent.tsx
+++ src/state/events/EventComponent.tsx
@@ -1,9 +1,12 @@
 export const EventComponent: React.FC = () => {
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event);
+    }
+
     return (
         <div>
-            <input onChange={e => {
-                console.log(e);
-            }} />
+            <input onChange={onChange} />
         </div>
     )
 }
